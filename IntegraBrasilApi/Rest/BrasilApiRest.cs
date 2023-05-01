925ac1e7e1480bbba63511c3da52cd50ea25b8b1
Bruno Marmello <brmarmello@gmail.com>
2023-05-01 01:52:48 -0300

    Dev: Creates API call to search address by zip code.

--- IntegraBrasilApi/Rest/BrasilApiRest.cs
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -1,14 +1,35 @@
+using System.Dynamic;
+using System.Text.Json;
 using IntegraBrasilApi.Dtos;
 using IntegraBrasilApi.Interfaces;
 using IntegraBrasilApi.Models;
 
 namespace IntegraBrasilApi.Rest
 {
-    public class BrasilApiRest
+    public class BrasilApiRest : IBrasilApi
     {
-        public Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCEP(string cep)
+        public async Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCEP(string cep)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://brasilapi.com.br/api/cep/v1/{cep}");
+
+            var response = new ResponseGenerico<EnderecoModel>();
+            using (var client = new HttpClient()) {
+                var responseBrasilApi = await client.SendAsync(request);
+                var contentResp = await responseBrasilApi.Content.ReadAsStringAsync();
+                var objResponse = JsonSerializer.Deserialize<EnderecoModel>(contentResp);
+
+                if (responseBrasilApi.IsSuccessStatusCode)
+                {
+                    response.CodigoHttp = responseBrasilApi.StatusCode;
+                    response.DadosRetorno = objResponse;
+                }
+                else
+                {
+                    response.CodigoHttp = responseBrasilApi.StatusCode;
+                    response.ErroRetorno = JsonSerializer.Deserialize<ExpandoObject>(contentResp);
+                }
+            }
+            return response;
         }
         Task<ResponseGenerico<List<BancoModel>>> BuscarTodosOsBancos()
         {
