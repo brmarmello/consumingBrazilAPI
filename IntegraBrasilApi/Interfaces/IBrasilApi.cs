a1951adddb15d646bf669fb4cef65ad5710a3635
Bruno Marmello <brmarmello@gmail.com>
2023-05-01 00:50:31 -0300

    Feat: Creates BrasilApi interface methods.

--- IntegraBrasilApi/Interfaces/IBrasilApi.cs
+++ IntegraBrasilApi/Interfaces/IBrasilApi.cs
@@ -1,7 +1,12 @@
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Models;
+
 namespace IntegraBrasilApi.Interfaces
 {
     public interface IBrasilApi
     {
-        
+        Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCEP(string cep);
+        Task<ResponseGenerico<List<BancoModel>>> BuscarTodosOsBancos();
+        Task<ResponseGenerico<BancoModel>> BuscarBanco(string codigoBanco);
     }
 }
